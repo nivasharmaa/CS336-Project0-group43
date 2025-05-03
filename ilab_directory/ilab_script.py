d99dbd2bb235b65e6cd6029e8baa3ae01fe0b4cf
Roman-Perry <roman.perry11@gmail.com>
2025-05-02 23:19:19 -0400

    Update ilab_script.py

--- ilab_directory/ilab_script.py
+++ ilab_directory/ilab_script.py
@@ -1,1 +1,30 @@
+import sys
+import psycopg2
+
+def run_query(query):
+    conn = psycopg2.connect(
+        dbname='rfp46',  # user specific database, your netID
+        user='rfp46',    # usename, just netID
+        password='Prophet622#',  # your password for ilab
+        host='postgres.cs.rutgers.edu' # the host psql database
+    )
+    cur = conn.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    colnames = [desc[0] for desc in cur.description]
+
+    # table formatting
+    print("\t".join(colnames))
+    for row in rows:
+        print("\t".join(str(col) for col in row))
+
+    cur.close()
+    conn.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        query = sys.argv[1]
+    else:
+        query = sys.stdin.read()
+    run_query(query)
 
